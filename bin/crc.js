398839c2c2a08d317c7b98d46b0655534d4929a1
Zac Smith <zrs3141592@gmail.com>
2025-11-21 08:42:15 -0700

    👍 IMPROVE: add update-notifier to provide notice that updates are available

--- bin/crc.js
+++ bin/crc.js
@@ -1,6 +1,31 @@
 #!/usr/bin/env node
 
+const updateNotifier = require('update-notifier');
+const chalk = require('chalk');
+const packageJson = require('../package.json');
 const { program } = require('../src/cli');
+
+// Check for updates (cached for 24 hours by default)
+const notifier = updateNotifier({
+  pkg: packageJson,
+  updateCheckInterval: 1000 * 60 * 60 * 24, // 24 hours
+});
+
+// Custom colorful update notification
+if (notifier.update) {
+  const { current, latest } = notifier.update;
+
+  console.log();
+  console.log(chalk.bgYellow.black.bold(' UPDATE AVAILABLE '));
+  console.log();
+  console.log(`  ${chalk.gray('Current version:')} ${chalk.red(current)}`);
+  console.log(`  ${chalk.gray('Latest version:')}  ${chalk.green.bold(latest)}`);
+  console.log();
+  console.log(`  ${chalk.cyan('Run:')} ${chalk.bold.white(`npm update -g ${packageJson.name}`)}`);
+  console.log();
+  console.log(chalk.gray('  ────────────────────────────────────────────────'));
+  console.log();
+}
 
 program.parse(process.argv);
 
